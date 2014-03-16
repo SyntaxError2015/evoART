9d20132bb1d84be5c4decdc2e0a60dad5ff0ba15
nbibest <neagubogdanioan@yahoo.com>
2014-03-16 23:40:18 +0200

    modificari design

---
+++ evoART/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using evoART.Models;
+
+namespace evoART.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public string Register()
+        {
+            return "hahahaa";
+        }
+
+
+
+        public string GetStatus()
+        {
+            return "Totul e OK la " + DateTime.Now.ToLongTimeString();
+        }
+
+
+        public string UpdateForm(string textBox1)
+        {
+            if (textBox1 != "Enter text")
+            {
+                return "Ai scris: \"" + textBox1.ToString() + "\" at " +
+                    DateTime.Now.ToLongTimeString();
+            }
+
+            return String.Empty;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(evoART.Models.LoginModel.Login model)
+        {
+            if (true)
+            {
+                return Redirect("/");
+            }
+
+            // If we got this far, something failed, redisplay form
+
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+    }
+}
---
+++ evoART/Models/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace evoART.Models
+{
+    public class LoginModel
+    {
+        public class Login
+        {
+            [Required]
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+        }
+    }
+}
