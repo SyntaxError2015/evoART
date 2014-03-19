c3e1f9a7365ee4156075198aba0337840f082cc1
Coddo <codobanclaudiu@gmail.com>
2014-03-19 21:21:40 +0200

    Added special functions module

--- evoART/Special/TextWarping.cs
+++ evoART/Special/TextWarping.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.UI.WebControls;
 
 namespace evoART.Special
 {
     public static class TextWarping
     {
-        public static string EncryptMD5(string key, CultureInfo cultureInfo)
+        public static string EncryptMD5(string key)
         {
-            throw new NotImplementedException();    
+            var cypher = MD5.Create();
+
+            // Calculate MD5 hash from input
+            var inputBytes = System.Text.Encoding.ASCII.GetBytes(key);
+            var hash = cypher.ComputeHash(inputBytes);
+
+            // Convert byte array to HEX string
+            var sb = new StringBuilder();
+
+            foreach (var part in hash)
+            {
+                sb.Append(part.ToString("X2"));
+            }
+
+            return sb.ToString();
         }
 
-        public static string ExtractHashKey(object obj, ParsingCulture parsingCulture)
+        public static string ExtractSpecialHashKey(object obj)
         {
-            throw new NotImplementedException();
+            return obj == null ? null : obj.GetHashCode().ToString(CultureInfo.InvariantCulture);
         }
     }
 }
