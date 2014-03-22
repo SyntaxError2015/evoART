9fad1389ac234954d4436cea529f9f5d2873dee7
Coddo <codobanclaudiu@gmail.com>
2014-03-23 01:08:55 +0200

    Added module for resizing images

---
+++ PhotoManipulator/ImageResizer.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace PhotoManipulator
+{
+    public static class ImageResizer
+    {
+        private const int MINIMUM_WIDTH = 1200;
+        private const int MINIMUM_HEIGHT = 800;
+        private const int THUMB_MIN_SIZE = 400;
+
+        private static Image ResizeImage(Image image, Size size)
+        {
+            if (image.Width < size.Width || image.Height < size.Height)
+                return image;
+
+            var bmpPhoto = new Bitmap(size.Width, size.Height, PixelFormat.Format24bppRgb);
+
+            var g = Graphics.FromImage(bmpPhoto);
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+
+            var rect = new Rectangle(0, 0, size.Width, size.Height);
+
+            g.DrawImage(image, rect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel);
+
+            image = bmpPhoto;
+
+            g.Dispose();
+
+            return image;
+        }
+
+        /// <summary>
+        /// Resize an image in order to reduce it's size in MB
+        /// </summary>
+        /// <param name="image">The Image entity to be resized</param>
+        /// <returns>An Image entity</returns>
+        public static Image ResizeImage(Image image)
+        {
+            var newSize = new Size();
+
+            if (image.Width > image.Height)
+            {
+                newSize.Height = MINIMUM_HEIGHT;
+                newSize.Width = (int)((image.Width) * ((float)newSize.Height / image.Height));
+            }
+
+            else
+            {
+                newSize.Width = MINIMUM_WIDTH;
+                newSize.Height = (image.Height) * (newSize.Width / image.Width);
+            }
+
+            return ResizeImage(image, newSize);
+        }
+
+        /// <summary>
+        /// Create a thumbnail for an image
+        /// </summary>
+        /// <param name="image">The Image entity to be resized</param>
+        /// <returns>An Image entity</returns>
+        public static Image CreateThumbnail(Image image)
+        {
+            var newSize = new Size();
+
+            if (image.Width > image.Height)
+            {
+                newSize.Height = THUMB_MIN_SIZE;
+                newSize.Width = (int)((image.Width) * ((float)newSize.Height / image.Height));
+            }
+
+            else
+            {
+                newSize.Width = THUMB_MIN_SIZE;
+                newSize.Height = (image.Height) * (newSize.Width / image.Width);
+            }
+
+            return ResizeImage(image, new Size(300, 200));
+        }
+    }
+}
---
+++ PhotoManipulator/UnitTesting.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhotoManipulator
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        private const string FILE = @"C:\Users\Coddo\Desktop\test.JPG";
+        private const string RESIZED = @"C:\Users\Coddo\Desktop\resized.JPG";
+        private const string THUMB = @"C:\Users\Coddo\Desktop\thumb.JPG";
+
+        [TestMethod]
+        public void ResizeImage()
+        {
+            var resizedImage = ImageResizer.ResizeImage(Image.FromFile(FILE));
+
+            if(File.Exists(RESIZED))
+                File.Delete(RESIZED);
+
+            resizedImage.Save(RESIZED, ImageFormat.Jpeg);
+        }
+
+        [TestMethod]
+        public void CreateThumbnail()
+        {
+            var thumbnail = ImageResizer.CreateThumbnail(Image.FromFile(FILE));
+
+            if (File.Exists(THUMB))
+                File.Delete(THUMB);
+
+            thumbnail.Save(THUMB, ImageFormat.Jpeg);
+        }
+    }
+}
