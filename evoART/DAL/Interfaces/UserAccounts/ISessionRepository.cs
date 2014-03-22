660d646a2a81195c7990e1195ef2721fe0e862c4
Coddo <codobanclaudiu@gmail.com>
2014-03-22 12:20:05 +0200

    Fixed missing Guid

--- evoART/DAL/Interfaces/UserAccounts/ISessionRepository.cs
+++ evoART/DAL/Interfaces/UserAccounts/ISessionRepository.cs
@@ -1,10 +1,11 @@
-﻿using evoART.Models.DbModels;
+﻿using System;
+using evoART.Models.DbModels;
 
 namespace evoART.DAL.Interfaces.UserAccounts
 {
     interface ISessionRepository
     {
-        AccountModels.UserAccount GetUser(int sessionId, string sessionKey);
+        AccountModels.UserAccount GetUser(Guid sessionId, string sessionKey);
 
         AccountModels.Session Login(string userName);
 
