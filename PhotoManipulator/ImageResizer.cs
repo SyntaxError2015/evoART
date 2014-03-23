34eee0cd489e036c2b93797a14154748db95de6a
Coddo <codobanclaudiu@gmail.com>
2014-03-23 17:43:52 +0200

    Added method for cropping images into a hexagon shape

--- PhotoManipulator/ImageResizer.cs
+++ PhotoManipulator/ImageResizer.cs
@@ -7,11 +7,19 @@ namespace PhotoManipulator
 {
     public static class ImageResizer
     {
+        #region Constant fields
+
         private const int MINIMUM_WIDTH = 1200;
         private const int MINIMUM_HEIGHT = 1000;
 
         private const int THUMB_MIN_SIZE_WIDTH = 400;
-        private const int THUMB_MIN_SIZE_HEIGTH = 400;
+        private const int THUMB_MIN_SIZE_HEIGTH = 300;
+
+        private const int HEXAGON_MIN_SIZE_UNIT = 200;
+
+        #endregion
+
+        #region Private methods
 
         private static Image ResizeImage(Image image, Size size)
         {
@@ -36,6 +44,45 @@ namespace PhotoManipulator
 
             return image;
         }
+
+        private static Image ResizeForHexagonShape(Image image)
+        {
+            var newSize = new Size();
+
+            if (image.Width > image.Height)
+            {
+                newSize.Height = HEXAGON_MIN_SIZE_UNIT;
+                newSize.Width = (int)((image.Width) * ((float)newSize.Height / image.Height));
+            }
+
+            else
+            {
+                newSize.Width = HEXAGON_MIN_SIZE_UNIT;
+                newSize.Height = (image.Height) * (newSize.Width / image.Width);
+            }
+
+            return ResizeImage(image, newSize);
+        }
+
+        private static PointF[] GenerateHexagonPointsForImage(Image image)
+        {
+            var radius = (int)((float)8 / 10 * ((image.Width > image.Height) ? image.Width / 3 : image.Height / 3));
+            var polygon = new PointF[6];
+
+            for (var i = 0; i < polygon.Length; i++)
+            {
+                var x = (float)(((float)image.Width / 2) + radius * Math.Cos(2 * Math.PI * i / polygon.Length));
+                var y = (float)(((float)image.Height / 2) + radius * Math.Sin(2 * Math.PI * i / polygon.Length));
+
+                polygon[i] = new PointF(x, y);
+            }
+
+            return polygon;
+        }
+
+        #endregion
+
+        #region Public methods
 
         /// <summary>
         /// Resize an image in order to reduce it's size in MB
@@ -86,37 +133,39 @@ namespace PhotoManipulator
             return ResizeImage(image, newSize);
         }
 
-        public static Image CreateHexagonShapedImage(Bitmap image)
+        /// <summary>
+        /// Crop an image into a hexagon shaped model, having its content start point as the center of the image
+        /// </summary>
+        /// <param name="image">The image to be cropped</param>
+        /// <returns>An Image entity</returns>
+        public static Image CreateHexagonImage(Image image)
         {
-            var img = new Bitmap(radius + 100, radius + 100);
-            
+            image = ResizeForHexagonShape(image);
+
+            var rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            var clip = new GraphicsPath();
+            clip.AddPolygon(GenerateHexagonPointsForImage(image));
+
+            var img = new Bitmap(image.Width, image.Height);
+
             var g = Graphics.FromImage(img);
-            var points = GenerateHexagonPointsForImage(image);
 
-            g.DrawPolygon(new Pen(Color.Pink), points);
-            g.DrawPolygon(new Pen(Color.Transparent), new PointF[] {new PointF(image.Width, 0), points[0], points[1]});
+            // Preset for high quality image
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-            image = img;
+            // Draw foreground image into clipping region
+            g.SetClip(clip, CombineMode.Replace);
+            g.DrawImage(image, rect);
+            g.ResetClip();
 
-            return image;
+            g.Save();
+
+            return img;
         }
 
-        private const int radius = 300;
-
-        private static PointF[] GenerateHexagonPointsForImage(Image image)
-        {
-            var polygon = new PointF[6];
-
-            for (var i = 0; i < polygon.Length; i++)
-            {
-                var x = (float) (((float)image.Width / 2) + radius * Math.Cos(2 * Math.PI * i / polygon.Length));
-                var y = (float) (((float)image.Height / 2) + radius * Math.Sin(2 * Math.PI * i / polygon.Length));
-
-                polygon[i] = new PointF(x, y);
-            }
-
-            return polygon;
-        }
-
+        #endregion
     }
 }
