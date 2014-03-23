fc064bec4bea29c0743639392d65e40a7a96b09e
Coddo <codobanclaudiu@gmail.com>
2014-03-23 02:01:40 +0200

    Fixed resizing ratio

--- PhotoManipulator/ImageResizer.cs
+++ PhotoManipulator/ImageResizer.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing.Imaging;
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Drawing;
 
 namespace PhotoManipulator
@@ -6,7 +8,7 @@ namespace PhotoManipulator
     public static class ImageResizer
     {
         private const int MINIMUM_WIDTH = 1200;
-        private const int MINIMUM_HEIGHT = 800;
+        private const int MINIMUM_HEIGHT = 1000;
         private const int THUMB_MIN_SIZE = 400;
 
         private static Image ResizeImage(Image image, Size size)
@@ -18,13 +20,14 @@ namespace PhotoManipulator
 
             var g = Graphics.FromImage(bmpPhoto);
 
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
             var rect = new Rectangle(0, 0, size.Width, size.Height);
-
+            
             g.DrawImage(image, rect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel);
+            //g.FillPolygon(new SolidBrush(Color.Red), HexagonPoints(image), FillMode.Winding);
 
             image = bmpPhoto;
 
@@ -44,14 +47,15 @@ namespace PhotoManipulator
 
             if (image.Width > image.Height)
             {
-                newSize.Height = MINIMUM_HEIGHT;
-                newSize.Width = (int)((image.Width) * ((float)newSize.Height / image.Height));
+                
+                newSize.Width = MINIMUM_WIDTH;
+                newSize.Height = (int) ((image.Height)*((float) newSize.Width/image.Width));
             }
 
             else
             {
-                newSize.Width = MINIMUM_WIDTH;
-                newSize.Height = (image.Height) * (newSize.Width / image.Width);
+                newSize.Height = MINIMUM_HEIGHT;
+                newSize.Width = (int)((image.Width) * ((float)newSize.Height / image.Height));
             }
 
             return ResizeImage(image, newSize);
@@ -79,6 +83,28 @@ namespace PhotoManipulator
             }
 
             return ResizeImage(image, new Size(300, 200));
+        }
+
+        public static Image CreateHexagonShapedImage(Image image)
+        {
+            return null; //return HexagonPoints(image);
+        }
+
+        private static Point[] HexagonPoints(Image image)
+        {
+            var points = new Point[6];
+
+            var half = image.Height/2;
+            var quart = image.Width/4;
+
+            points[0] = new Point(quart, 0);
+            points[1] = new Point(image.Width - quart, 0);
+            points[2] = new Point(image.Width, half);
+            points[3] = new Point(image.Width - quart, image.Height);
+            points[4] = new Point(quart, image.Height);
+            points[5] = new Point(0, half);
+
+            return points;
         }
     }
 }
