495cc5b68199a7b9a601c57d7bc0e141bea60970
Coddo <codobanclaudiu@gmail.com>
2014-03-23 16:24:52 +0200

    Added test for drawing hex image

--- UnitTesting/PhotomanipTests.cs
+++ UnitTesting/PhotomanipTests.cs
@@ -12,6 +12,7 @@ namespace UnitTesting
         private const string FILE = @"C:\Users\Coddo\Desktop\test.JPG";
         private const string RESIZED = @"C:\Users\Coddo\Desktop\resized.JPG";
         private const string THUMB = @"C:\Users\Coddo\Desktop\thumb.JPG";
+        private const string HEX = @"C:\Users\Coddo\Desktop\hex.png";
 
         [TestMethod]
         public void ResizeImage()
@@ -33,6 +34,17 @@ namespace UnitTesting
                 File.Delete(THUMB);
 
             thumbnail.Save(THUMB, ImageFormat.Jpeg);
+        }
+
+        [TestMethod]
+        public void CreateHexThumbnail()
+        {
+            var hex = ImageResizer.CreateThumbnail(Image.FromFile(FILE));
+
+            if (File.Exists(HEX))
+                File.Delete(HEX);
+
+            hex.Save(HEX, ImageFormat.Png);
         }
     }
 }
