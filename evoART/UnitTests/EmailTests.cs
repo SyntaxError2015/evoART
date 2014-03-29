56372a37d873f26fde7388a141f418abec6f0a5a
Coddo <codobanclaudiu@gmail.com>
2014-03-29 12:52:46 +0200

    Added unit tests for emails

---
+++ evoART/UnitTests/EmailTests.cs
@@ -0,0 +1,21 @@
+﻿using evoART.Emails;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace evoART.UnitTests
+{
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        public void SendTestRegistrationEmail()
+        {
+            EmailSender.SendRegistrationEmail("TESTVALIDATIONTOKENASHJASHFASJFHASJFASF", "codobanclaudiu@gmail.com");
+        }
+
+        [TestMethod]
+        public void SendTestPasswordResetEmail()
+        {
+            
+        }
+    }
+}
